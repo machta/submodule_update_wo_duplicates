725352229edfbcdb478cf253a3fafc466b23a730
Martin Bárta <martin.barta@hotmail.com>
2021-02-06 18:19:15 +0100

    Add some tests.

--- submodule_update_wo_duplicates.py
+++ submodule_update_wo_duplicates.py
@@ -7,7 +7,7 @@ import subprocess, git, os
 def bash(cmd, cwd=None):
     # print("+ bash:", cwd, cmd)
     return subprocess.run(["bash", "-c", cmd], #stderr=subprocess.DEVNULL,
-        stdout=subprocess.PIPE, check=True, cwd=cwd).stdout.decode('ascii')
+        stdout=subprocess.PIPE, check=True, cwd=cwd).stdout.decode('utf-8')
 
 # to make the output nice and aligned
 LINK_MSG   = "linking  "
@@ -22,7 +22,16 @@ def replace_by_link(src, dst):
     # os.symlink(src, dst) # can't do relative links (the -r switch)
 
 # FIXME doesn't work if change in a direct submodule is only staged, but not committed
-def update_one_level(mod = ".", cloned_mods = {}):
+def update_one_level(mod = ".", cloned_mods = None):
+    if not cloned_mods:
+        # TODO Possible optimization: on the first level do a non-recursive update
+        # on the whole repo. You will have to update all of them anyway.
+        # Then just save all the hashes and URIs and you're done with the first level.
+        # Doing the update in a batch will make it swifter.
+        # TODO Possible optimization: If hash before and after the update doesn't change
+        # for one repo, you don't have to recurse into the module at all.
+        # Do this for both the update at the start and for the recursive ones after that as well.
+        cloned_mods = {}
     recurse_into = []
     for m in git.Repo(mod).submodules:
         m_path = os.path.join(mod, m.path)
@@ -42,8 +51,8 @@ def update_one_level(mod = ".", cloned_mods = {}):
         update_one_level(m, cloned_mods)
 
 
-
-update_one_level()
+if __name__ == "__main__":
+    update_one_level()
 
 # update() doc:
 # https://github.com/gitpython-developers/GitPython/blob/24f75e7bae3974746f29aaecf6de011af79a675d/git/objects/submodule/base.py#L444
---
+++ tests/test_basic.py
@@ -0,0 +1,176 @@
+import unittest, sys, os
+import tempfile
+
+sys.path.append(os.path.abspath(__file__ + "/../.."))
+from submodule_update_wo_duplicates import bash, update_one_level
+
+def git_commit(repo, commit):
+    bash("git add -A", repo)
+    bash(f"git commit -m {commit}", repo)
+    bash(f"git tag {commit}", repo)
+
+def init_repo(repo):
+    bash(f"git init {repo}")
+    bash(f"echo 'repo {repo}' > file", repo)
+    git_commit(repo, "c1")
+    bash(f"echo 'repo {repo}' > file2", repo)
+    git_commit(repo, "c2")
+
+def add_submod(repo, sub_path, submod):
+    bash(f"git submodule add {os.path.abspath(submod)} {sub_path}", repo)
+    git_commit(repo, f"add-{submod}")
+
+def make_3_repos():
+    init_repo("git1")
+    init_repo("git2")
+    init_repo("git3")
+
+    add_submod("git2", "libs/git3", "git3")
+    add_submod("git1", "libs/git3", "git3")
+    add_submod("git1", "libs/git2", "git2")
+
+
+def repo_content(path):
+    return bash("find -L -type f | grep -v '\\.git' | sort | xargs tail -n 1000", path)
+
+def clean_clone_content(repo, rev):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        bash(f"git clone {os.path.abspath('git1')} {tmpdir}")
+        bash(f"git checkout {rev}", tmpdir)
+        bash(f"git submodule update --init --recursive", tmpdir)
+        content = repo_content(tmpdir)
+        return content
+
+
+def call_update(repo):
+    os.chdir(repo)
+    update_one_level()
+    os.chdir("..")
+
+
+class TestBasic(unittest.TestCase):
+
+    def setUp(self):
+        self.tmpdiro = tempfile.TemporaryDirectory()
+        self.tmpdir = self.tmpdiro.name
+        # unlike TemporaryDirectory() doesn't remove the dir in dtor
+        # self.tmpdir = tempfile.mkdtemp()
+        os.chdir(self.tmpdir)
+
+        make_3_repos()
+
+        print()
+        print("========================================>")
+        print()
+
+    def tearDown(self):
+        print()
+        print("<========================================")
+        print()
+        print(bash(f"tree {self.tmpdir}"))
+        pass
+
+
+    def test_basic_wo_modules(self):
+        bash(f"git clone {os.path.abspath('git1')} clone_git1")
+
+        bash(f"git checkout c2", "clone_git1")
+        call_update("clone_git1")
+
+        linked_content = repo_content("clone_git1")
+        expected_content = clean_clone_content("git1", "c2")
+        self.assertEqual(linked_content, expected_content)
+
+
+    def test_basic_w_modules(self):
+        bash(f"git clone {os.path.abspath('git1')} clone_git1")
+
+        bash(f"git checkout add-git2", "clone_git1")
+        call_update("clone_git1")
+
+        linked_content = repo_content("clone_git1")
+        expected_content = clean_clone_content("git1", "add-git2")
+        self.assertEqual(linked_content, expected_content)
+
+
+    """
+    Tests that if a previously linked repo can be turned into a real module again.
+    This requires force for update() because the link made previously makes the repo 'dirty'.
+    """
+    def test_submod_downgrade(self):
+        bash(f"git checkout c1", "git2/libs/git3")
+        git_commit("git2", "downgrade-git3")
+
+        bash(f"git fetch", "git1/libs/git2")
+        bash(f"git checkout downgrade-git3", "git1/libs/git2")
+        git_commit("git1", "downgrade-git3")
+
+        bash(f"git clone {os.path.abspath('git1')} clone_git1")
+
+        bash(f"git checkout add-git2", "clone_git1")
+        call_update("clone_git1")
+        bash(f"git checkout downgrade-git3", "clone_git1")
+        call_update("clone_git1")
+
+        linked_content = repo_content("clone_git1")
+        expected_content = clean_clone_content("git1", "downgrade-git3")
+        self.assertEqual(linked_content, expected_content)
+
+
+    """
+    The same as test_submod_downgrade but adds an extra step that goes back to the
+    state when the repo is linked again.
+    """
+    def test_submod_link_update_link(self):
+        bash(f"git checkout c1", "git2/libs/git3")
+        git_commit("git2", "downgrade-git3")
+
+        bash(f"git fetch", "git1/libs/git2")
+        bash(f"git checkout downgrade-git3", "git1/libs/git2")
+        git_commit("git1", "downgrade-git3")
+
+        bash(f"git clone {os.path.abspath('git1')} clone_git1")
+
+        bash(f"git checkout add-git2", "clone_git1")
+        call_update("clone_git1")
+        bash(f"git checkout downgrade-git3", "clone_git1")
+        call_update("clone_git1")
+        bash(f"git checkout add-git2", "clone_git1")
+        call_update("clone_git1")
+
+        linked_content = repo_content("clone_git1")
+        expected_content = clean_clone_content("git1", "add-git2")
+        self.assertEqual(linked_content, expected_content)
+
+
+    """
+    Tests that a cloned repo, subsequently replaced by a link, can be cloned again.
+    This makes update() fail when the submod 'already exists' in .git/modules directory.
+
+    This used to be a bug on an early version of this tool. An extra step is necessary
+    when unlinking to make sure the appropriate directory in .git/modules is cleaned as well.
+    """
+    def test_submod_update_link_update(self):
+        bash(f"git checkout c1", "git2/libs/git3")
+        git_commit("git2", "downgrade-git3")
+
+        bash(f"git fetch", "git1/libs/git2")
+        bash(f"git checkout downgrade-git3", "git1/libs/git2")
+        git_commit("git1", "downgrade-git3")
+
+        bash(f"git clone {os.path.abspath('git1')} clone_git1")
+
+        bash(f"git checkout downgrade-git3", "clone_git1")
+        call_update("clone_git1")
+        bash(f"git checkout add-git2", "clone_git1")
+        call_update("clone_git1")
+        bash(f"git checkout downgrade-git3", "clone_git1")
+        call_update("clone_git1")
+
+        linked_content = repo_content("clone_git1")
+        expected_content = clean_clone_content("git1", "downgrade-git3")
+        self.assertEqual(linked_content, expected_content)
+
+
+if __name__ == '__main__':
+    unittest.main()
